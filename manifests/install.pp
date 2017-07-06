26413e089dea2f9073d3b7e13ab46a81a21f68c4
Benoît Clouet <benoitclouet@localhost.localdomain>
2017-07-06 20:13:32 +0200

    Initial commit. Only supports install so far

---
+++ manifests/install.pp
@@ -0,0 +1,27 @@
+# == Class: geth::install
+class geth::install inherits geth {
+
+  yumrepo { 'okay-repo':
+    baseurl => 'http://repo.okay.com.mx/centos/$releasever/$basearch/release',
+    descr => 'OKAY RPM repository',
+    enabled => 1,
+    gpgcheck => 1,
+    gpgkey => '/etc/pki/rpm-gpg/RPM-GPG-KEY-OKAY',
+  }
+
+  package { 'go-ethereum':
+    ensure => installed,
+    require => Yumrepo["okay-repo"],
+  }
+
+  group { 'geth':
+    ensure => 'present',
+  }
+
+  user { 'geth':
+    ensure => 'present',
+    groups => 'geth',
+  }
+
+}
+
