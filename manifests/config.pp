093b805260afb9d1bbc6ae1c95f49f911a13561d
Benoît Clouet <benoitclouet@MacBook-Pro-de-Benoit.local>
2017-07-09 15:56:27 -0400

    Added some logs to config commands.

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,12 +3,15 @@ class geth::config (
   String  $datadir          = $geth::datadir,
   String  $account_password = $geth::account_password,
   String  $passfile         = $geth::passfile,
+  String  $logdir           = $geth::logdir,
 )
 inherits geth {
   exec { 'geth-init-data':
     cwd     => '/home/geth',
-    command => "geth data init --datadir ${datadir}",
+    command => "geth data init --datadir ${datadir} 2>> ${logdir}/init-data.log",
     user    => 'geth',
+    # Command is so long ?!?
+    timeout => 1800,
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
 }
 ~> file { 'passfile':
@@ -19,7 +22,7 @@ inherits geth {
 }
 ~> exec { 'geth-create-account':
     cwd     => '/home/geth',
-    command => "geth account new --password ${passfile}",
+    command => "geth account new --password ${passfile} 2>> ${logdir}/create-account.log",
     user    => 'geth',
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
   }
