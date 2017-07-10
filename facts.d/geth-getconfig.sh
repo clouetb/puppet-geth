653d41eae44382b7192a602067d3ca700d651df6
Benoît Clouet <benoitclouet@MacBook-Pro-de-Benoit.local>
2017-07-10 12:59:45 -0400

    Added some sanity checks.

--- facts.d/geth-getconfig.sh
+++ facts.d/geth-getconfig.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+
+# Ethereum seems not to be properly installed
+if [ ! -f /lib/systemd/system/geth.service ]; then
+    echo -e "{}"
+    exit 0
+fi
+
+ETH_PID=$(pidof eth)
+
+# Ethereum seems not to be running
+if [ -z $ETH_PID ]; then
+    echo -e "{}"
+    exit 0
+fi
 DATADIR=$(grep datadir /lib/systemd/system/geth.service |cut --delimiter '"' --fields 2)
 IPC="ipc:$(find $DATADIR -name geth.ipc)"
 
-function fact {
-    geth --exec "$1" attach $IPC
-}
+NODE_INFO=$(geth --exec "admin.nodeInfo" attach $IPC)
+ETH=$(geth --exec "eth" attach $IPC)
 
-nodeInfo=$(fact "admin.nodeInfo")
-eth=$(fact "eth")
-
-echo -e " {\n  nodeInfo: $nodeInfo,\n  eth: $eth\n}"
+echo -e "{\n  ethPid: $ETH_PID,\n nodeInfo: $NODE_INFO,\n  eth: $ETH\n}"
