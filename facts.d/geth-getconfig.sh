72b842c7115337b94a233cd41298c5129fa678f5
Benoît Clouet <benoitclouet@MacBook-Pro-de-Benoit.local>
2017-07-10 14:39:55 -0400

    Added layout.

--- facts.d/geth-getconfig.sh
+++ facts.d/geth-getconfig.sh
@@ -2,15 +2,17 @@
 
 # Ethereum seems not to be properly installed
 if [ ! -f /lib/systemd/system/geth.service ]; then
+    # Nothing to return
     echo -e "{}"
     exit 0
 fi
 
-ETH_PID=$(pidof eth)
+ETH_PID=$(pidof geth)
 
 # Ethereum seems not to be running
 if [ -z $ETH_PID ]; then
-    echo -e "{}"
+    # Return empty information
+    echo -e "go-ethereum: {}"
     exit 0
 fi
 DATADIR=$(grep datadir /lib/systemd/system/geth.service |cut --delimiter '"' --fields 2)
@@ -19,4 +21,4 @@ IPC="ipc:$(find $DATADIR -name geth.ipc)"
 NODE_INFO=$(geth --exec "admin.nodeInfo" attach $IPC)
 ETH=$(geth --exec "eth" attach $IPC)
 
-echo -e "{\n  ethPid: $ETH_PID,\n nodeInfo: $NODE_INFO,\n  eth: $ETH\n}"
+echo -e "go-ethereum: {\n  gethPid: $ETH_PID,\n nodeInfo: $NODE_INFO,\n  eth: $ETH\n}"
